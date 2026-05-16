1e78848ec42ad33fd37b8ecf4fc90ea7daffb52c
AndrewKiran25 <director.ashok888@gmail.com>
2026-05-16 11:35:07 +0530

    Update app.py

--- llm/deployment/app.py
+++ llm/deployment/app.py
@@ -45,7 +45,7 @@ st.markdown(
 def load_embedding_model():
 
     embedding_model = HuggingFaceEmbeddings(
-    model_name="thenlper/gte-large"
+    model_name="sentence-transformers/all-MiniLM-L6-v2"
 )
 
     return embedding_model
