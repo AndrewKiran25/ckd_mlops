38ab2e1553c0597fbbc7e5619365324367c2c992
AndrewKiran25 <director.ashok888@gmail.com>
2026-05-17 10:07:11 +0530

    Update train.py

--- llm/model_building/train.py
+++ llm/model_building/train.py
@@ -72,6 +72,13 @@ embedding_model = HuggingFaceEmbeddings(
     model_name=EMBEDDING_MODEL_NAME
 )
 
+embedding_model = HuggingFaceEmbeddings(
+    model_name=EMBEDDING_MODEL_NAME,
+    encode_kwargs={
+        "normalize_embeddings": True
+    }
+)
+
 print("Embedding model loaded successfully.")
 
 # =========================================================
