dcbe53d7721859c5f2f23ac9732a71217bf25f80
AndrewKiran25 <director.ashok888@gmail.com>
2026-05-17 10:47:26 +0530

    Update train.py

--- llm/model_building/train.py
+++ llm/model_building/train.py
@@ -12,10 +12,8 @@ warnings.filterwarnings("ignore")
 # LANGCHAIN IMPORTS
 # =========================================================
 
-from langchain_community.vectorstores import Chroma
-
+from langchain_chroma import Chroma
 from langchain.prompts import PromptTemplate
-
 from langchain_huggingface import (
     HuggingFaceEmbeddings
 )
